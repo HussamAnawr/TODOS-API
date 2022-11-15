f5664ac8f42830b1e12bbed0a8ed61780eea18a7
Hussam <o.hussam@gmail.com>
2022-11-15 22:50:57 +0300

    Final basic todo api

--- todos/tests/test_todo.py
+++ todos/tests/test_todo.py
@@ -1,5 +1,8 @@
 from django.test import TestCase
 from ..models import Todo
+from rest_framework import status
+from rest_framework.test import APITestCase
+from django.urls import reverse
 
 class TodoModelTests(TestCase):
     @classmethod
@@ -12,3 +15,14 @@ class TodoModelTests(TestCase):
         self.assertEqual(self.todo.title, 'First Todo')
         self.assertEqual(self.todo.body, 'A body of text here')
         self.assertEqual(str(self.todo), 'First Todo')
+
+    def test_api_listview(self):
+        res = self.client.get(reverse('todos:todo_list'))
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(Todo.objects.count(), 1)
+        self.assertContains(res, self.todo)
+
+    def test_api_detailview(self):
+        res = self.client.get(reverse('todos:todo_detail', kwargs={'pk':self.todo.id}), format='json')
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertContains(res, 'First Todo')
---
+++ todos/views.py
@@ -0,0 +1,11 @@
+from .serializers import TodoSerializer
+from rest_framework import generics
+from .models import Todo
+
+class ListTodo(generics.ListAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+class DetailTodo(generics.RetrieveAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
